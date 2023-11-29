b8dabcc30a319ee380816658a0c6e557b3e8deb8
okhan1998 <whwlgns111@gmail.com>
2023-11-29 19:28:04 +0900

    feat: 스타일 파레트 설정 (글꼴, 색깔, z-index)
    Co-authored-by: Yu Yeon ah <yeonah98@users.noreply.github.com>
    Co-authored-by: 허민 (Her Min) <hermin9804@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-// import Image from 'next/image';
+import styles from './main.module.scss';
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return;
+  <>
+    <div className={styles.first}>Hello World</div>
+    <div className="second">Hello World</div>
+    <div className="third">Hello World</div>
+    <h1>Hello World</h1>
+    <div>Hello World</div>
+    <div>Hello World</div>
+    <div>Hello World</div>
+    <div>Hello World</div>
+    <div>Hello World</div>
+    <div>Hello World</div>
+    <div>Hello World</div>
+    <div>Hello World</div>
+  </>;
 }
