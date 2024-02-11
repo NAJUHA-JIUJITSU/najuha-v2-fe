5a838bd30345a1cfc89fcd823cc1c078d3f9e094
hermin9804 <hermin9804@gmail.com>
2024-02-11 16:54:46 +0900

    refactor: PhoneNumber 컴포넌트 리팩토링

--- components/register/phonenumber/index.tsx
+++ components/register/phonenumber/index.tsx
@@ -1,56 +1,81 @@
-'use client';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Input from '@/components/common/input';
-import stlyes from './index.module.scss';
+import styles from './index.module.scss';
 import ButtonOnClick from '@/components/common/button/buttonOnClick';
+import { useRecoilState } from 'recoil';
+import { userAtom } from '@/recoil/userAtom';
 
-export default function Phonenumber({ onNext }: any) {
-  const [phonenumber, setPhonenumber] = useState<string>('');
-  const [phonenumberErrMsg, setPhonenumberErrMsg] = useState<string | null>(null);
+interface Props {
+  onNext: () => void;
+}
 
-  //validateBirth 함수
-  const validatePhonenumber = (inputPhonenumber: string) => {
-    // 입력된 값에서 숫자만 추출
-    const numericValue = inputPhonenumber.replace(/[^0-9]/g, '');
+enum PhoneValidationState {
+  EMPTY,
+  TOO_SHORT,
+  VALID,
+}
 
-    // 숫자를 'YYYY/MM/DD' 형식으로 변환
-    const formattedPhonenumber =
-      numericValue.slice(0, 3) +
-      (numericValue.length > 3 ? '-' + numericValue.slice(3, 7) : '') +
-      (numericValue.length > 7 ? '-' + numericValue.slice(7, 11) : '');
+const ErrorMsgMap = {
+  [PhoneValidationState.EMPTY]: null,
+  [PhoneValidationState.TOO_SHORT]: '휴대폰 번호 11자리를 입력해주세요.',
+  [PhoneValidationState.VALID]: null,
+};
 
-    // 실시간으로 형식에 맞게 업데이트
-    setPhonenumber(formattedPhonenumber);
+const formatPhoneNumber = (inputPhone: string) => {
+  const numericValue = inputPhone.replace(/[^0-9]/g, '');
+  return (
+    numericValue.slice(0, 3) +
+    (numericValue.length > 3 ? '-' + numericValue.slice(3, 7) : '') +
+    (numericValue.length > 7 ? '-' + numericValue.slice(7) : '')
+  );
+};
 
-    // 11자리 미만일때 에러메시지
-    if (numericValue.length < 11 && numericValue.length > 0) {
-      setPhonenumberErrMsg('휴대폰 번호 11자리를 입력해주세요.');
-      return false;
-    }
+const validatePhoneNumber = (phone: string): PhoneValidationState => {
+  const numericValue = phone.replace(/[^0-9]/g, '');
+  if (numericValue === '') return PhoneValidationState.EMPTY;
+  if (numericValue.length < 11) return PhoneValidationState.TOO_SHORT;
+  return PhoneValidationState.VALID;
+};
 
-    setPhonenumberErrMsg(null);
+// TODO: 전화번호 인증 로직 추가
+const PhoneNumber = ({ onNext }: Props) => {
+  const [user, setUser] = useRecoilState(userAtom);
+  const [phoneNumber, setPhoneNumber] = useState<string>(user.phoneNumber || '');
+  const [phoneValidationState, setPhoneValidationState] = useState<PhoneValidationState>(
+    PhoneValidationState.EMPTY,
+  );
+
+  const phoneInputHandler = (inputPhone: string) => {
+    const formattedPhone = formatPhoneNumber(inputPhone);
+    const validationState = validatePhoneNumber(formattedPhone);
+    setPhoneNumber(formattedPhone);
+    setPhoneValidationState(validationState);
+    setUser((prevUser) => ({
+      ...prevUser,
+      phone: formattedPhone.replace(/-/g, ''),
+    }));
   };
 
   useEffect(() => {
-    validatePhonenumber(phonenumber);
-  }, [phonenumber]);
+    phoneInputHandler(phoneNumber);
+  }, [phoneNumber]);
 
   return (
     <>
-      <div className={stlyes.wrapper}>
+      <div className={styles.wrapper}>
         <Input
           label="휴대폰 번호를 입력해주세요"
-          placeholder="010-1234-1234"
-          value={phonenumber}
-          onChange={(e) => setPhonenumber(e.target.value)}
-          errMsg={phonenumberErrMsg}
+          placeholder="010-1234-5678"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          errMsg={ErrorMsgMap[phoneValidationState]}
         />
       </div>
-      <div className={stlyes.submit}>
+      <div className={styles.submit}>
         <ButtonOnClick
           type="filled"
-          text="약관전체 동의"
-          color="blue"
+          text="전화번호 인증"
+          color={phoneValidationState === PhoneValidationState.VALID ? 'blue' : 'disabled'}
           width="full"
           size="large"
           onClick={onNext}
@@ -58,4 +83,6 @@ export default function Phonenumber({ onNext }: any) {
       </div>
     </>
   );
-}
+};
+
+export default PhoneNumber;
