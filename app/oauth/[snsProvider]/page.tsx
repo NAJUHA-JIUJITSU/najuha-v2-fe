0f184d81b46f27502f75218afc13c6ab5faf571f
okhan1998 <whwlgns111@gmail.com>
2024-03-11 19:17:26 +0900

    feat: oauth snsProvider페이지 useEffect조건 리펙토링

--- app/oauth/[snsProvider]/page.tsx
+++ app/oauth/[snsProvider]/page.tsx
@@ -15,20 +15,22 @@ export default function SnsRedirectPage({ params, searchParams }: SnsRedirectPag
   const router = useRouter();
 
   useEffect(() => {
-    if (!isLoading && !error && payload) {
-      //todo: switch문으로 변경
+    if (error) {
+      console.error('SNS 로그인 에러:', error);
+      router.push('/login');
+      return;
+    }
+    if (payload) {
       const { userRole } = payload;
       if (userRole === 'TEMPORARY_USER') {
         router.push(`/funnel`);
       } else {
+        // 리프레시 토큰이 만료되어 왔으면 이전페이지로 리다이렉트해야함
         console.log('로그인된 회원입니다');
-        router.push(`/funnel`); //todo: 이전 페이지로 리다이렉트
+        router.push('/');
       }
-    } else if (!isLoading && error) {
-      console.error('로그인 에러:', error);
-      router.push('/login');
     }
-  }, [isLoading, payload, error]);
+  }, [error, payload]);
 
   if (isLoading) {
     return <div className={styles.wrapper}>Loading...</div>;
@@ -36,104 +38,3 @@ export default function SnsRedirectPage({ params, searchParams }: SnsRedirectPag
 
   return <div className={styles.wrapper}>Redirecting...</div>;
 }
-
-// 'use client';
-// import { useEffect, useState } from 'react';
-// import { jwtDecode } from 'jwt-decode';
-// import { useRouter } from 'next/navigation';
-// import styles from './index.module.scss';
-
-// interface SnsRedirectPageProps {
-//   params: { snsProvider: string };
-//   searchParams: { code: string };
-// }
-
-// interface MyTokenPayload {
-//   userId: number;
-//   userRole: string;
-//   iat: number;
-//   exp: number;
-// }
-
-// export default function SnsRedirectPage({ params, searchParams }: SnsRedirectPageProps) {
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   const router = useRouter();
-
-//   // 토큰 디코드를 위한 함수
-//   function decodeToken(token: string): MyTokenPayload | null {
-//     try {
-//       const decoded = jwtDecode<MyTokenPayload>(token);
-//       console.log(decoded); // 디코드된 페이로드 출력
-//       return decoded; // 디코드된 객체 반환
-//     } catch (error) {
-//       console.error('토큰 디코드 실패:', error);
-//       return null;
-//     }
-//   }
-
-//   async function handleSnsLogin(snsAuthProvider: string, snsAuthCode: string) {
-//     setIsLoading(true);
-//     try {
-//       const response = await fetch('http://localhost:3001/auth/snsLogin', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//           snsAuthProvider,
-//           snsAuthCode,
-//         }),
-//       });
-
-//       if (!response.ok) {
-//         throw new Error('SNS 로그인 실패');
-//       }
-
-//       const data = await response.json(); // 서버로부터 응답 받기
-
-//       const payload = decodeToken(data.data.accessToken); // 토큰 디코드
-//       const userId = payload?.userId; // 디코드된 페이로드에서 유저 정보 가져오기
-//       const userRole = payload?.userRole; // 디코드된 페이로드에서 유저 정보 가져오기
-
-//       // TEMPORARY_USER일 경우 회원가입 페이지로 userId와 함께 리디렉션
-//       if (userRole === 'TEMPORARY_USER') {
-//         console.log('회원가입 리디렉션, userId:  ' + userId);
-//         router.push('/funnel?userId=' + userId);
-//       } else {
-//         // 회원일 경우, 토큰 저장 후 메인 페이지로 리디렉션
-//         console.log('토큰 저장 후 메인 페이지 리디렉션');
-//         router.push('/');
-//       }
-//     } catch (error) {
-//       // 로그인 실패 시 로그인 페이지로 이동
-//       console.error('SNS 로그인 에러:', error);
-//       router.push('/login');
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   }
-
-//   useEffect(() => {
-//     const snsAuthProvider = params.snsProvider;
-//     const snsAuthCode = searchParams.code;
-//     console.log('snsAuthProvider: ', params.snsProvider);
-//     console.log('snsAuthCode: ', searchParams.code);
-
-//     if (!snsAuthProvider || !snsAuthCode) {
-//       console.error('SNS 로그인 정보가 없습니다.');
-//       return;
-//     }
-
-//     if (isLoading && snsAuthProvider && snsAuthCode) {
-//       handleSnsLogin(snsAuthProvider, snsAuthCode);
-//     }
-//   }, [params, searchParams, isLoading]);
-
-//   if (isLoading) {
-//     return <div className={styles.wrapper}>Loading...</div>; // 로딩 중 UI
-//   }
-
-//   // 로딩 완료 후 UI (필요 시 추가 구현)
-//   return <div className={styles.wrapper}>Redirecting...</div>;
-// }
