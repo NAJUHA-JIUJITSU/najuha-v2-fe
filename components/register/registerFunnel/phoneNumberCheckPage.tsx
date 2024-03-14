ad369eb1013421d28a34f4d16393a32af562c08b
okhan1998 <whwlgns111@gmail.com>
2024-03-15 02:30:40 +0900

    feat:(오칸코드추가) phoneNumberCheckPage에 useinput && recoil적용

--- components/register/registerFunnel/phoneNumberCheckPage.tsx
+++ components/register/registerFunnel/phoneNumberCheckPage.tsx
@@ -1,99 +1,88 @@
 'use client';
-import Input from '../../common/input';
-import { useEffect, useState } from 'react';
-import styles from './index.module.scss';
-import ButtonOnClick from '../../common/button/buttonOnClick';
-import React from 'react';
-import usePhoneNumberCodeValidation from '@/hook/usePhoneNumberCodeValidation';
+import { useState, useCallback } from 'react';
+import Input from '@/components/common/input';
+import stlyes from './index.module.scss';
+import ButtonOnClick from '@/components/common/button/buttonOnClick';
+import { useInput } from '@/hook/useInput';
+import { validateVerificationNumber } from '@/utils/validations/userValidations';
 import { useConfirmAuthCode, useSendAuthCode } from '@/hook/useRegister';
+import { useRecoilValue } from 'recoil';
+import { phoneNumberState } from '@/recoil/atoms/registerState';
+import { useTimer } from '@/hook/useTimer';
 
-interface PhoneNumberCheckPageProps {
-  onNext: (data: boolean) => void;
-  data: string;
-}
+export default function Verify({ onNext }: any) {
+  const { value, setValue, errMsg, validate, setErrMsg, setSuccessMsg, successMsg } = useInput(
+    '',
+    validateVerificationNumber,
+  );
+  const { mutate, isPending } = useConfirmAuthCode();
+  const { mutate: getAuthCode, isPending: isLoading } = useSendAuthCode(); //todo: isPending 사용해서 버튼 비활성화
+  const { isTimeOver, resetTimer, formatTime } = useTimer();
+  const phoneNumber = useRecoilValue(phoneNumberState);
 
-export default function phoneNumberCheckPage({
-  onNext,
-  data: phoneNumber,
-}: PhoneNumberCheckPageProps) {
-  const {
-    code,
-    setCode,
-    isValid,
-    errorMessage,
-    setErrorMessage,
-    isTimerActive,
-    resetTimer,
-    timeLeft,
-  } = usePhoneNumberCodeValidation();
-  const { mutate: confirmAuthCode, isPending } = useConfirmAuthCode();
-  const { mutate: sendAuthCode } = useSendAuthCode(); //todo: isPending 사용해서 버튼 비활성화
-
-  const handleButtonClick = () => {
-    confirmAuthCode(code, {
-      onSuccess: (response) => {
-        if (response.data.result) {
-          onNext(true);
-        }
-        setErrorMessage('인증코드가 일치하지 않습니다.');
-      },
-      onError: (error) => {
-        console.log('인증코드 확인 실패: ', error);
-      },
-    });
-  };
-
-  const handleAgainButtonClick = () => {
-    sendAuthCode(phoneNumber, {
-      onSuccess: (response) => {
-        console.log('인증코드 전송 성공: ', response.data.result);
+  const handleGetAuthCode = useCallback(() => {
+    getAuthCode(phoneNumber, {
+      onSuccess: (res) => {
+        console.log(res);
         resetTimer();
       },
-      onError: (error) => {
-        console.log('인증코드 전송 실패: ', error);
+      onError: () => {
+        setErrMsg('인증번호 발송중 오류가 발생했습니다.');
       },
     });
+  }, []);
 
-    console.log('다시 받기 버튼 클릭');
+  const handleButtonClick = () => {
+    mutate(value, {
+      onSuccess: (res) => {
+        if (res.data.result) {
+          onNext();
+        } else {
+          setErrMsg('인증번호가 올바르지 않습니다.');
+        }
+      },
+      onError: (error: any) => {
+        console.log(error);
+        setErrMsg('인증번호 확인 중 오류가 발생했습니다.');
+      },
+    });
   };
 
   return (
-    <div className={styles.wrapper}>
-      {/* 인증번호 번호 입력 */}
-      <Input
-        label="인증번호를 입력해주세요"
-        placeholder="6자리 인증번호"
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-        errMsg={errorMessage}
-        disabled={isPending}
-        autoFocus={true}
-      />
-
-      {/* 다시 받기 버튼 */}
-      <div className={styles.validateButton}>
-        <p className={styles.timer}>{timeLeft}</p>
-        <ButtonOnClick
-          type="filled"
-          text="다시 받기"
-          color={isTimerActive ? 'disabled' : 'lightblue'}
-          width="full"
-          size="small"
-          onClick={() => handleAgainButtonClick()}
+    <>
+      <div className={stlyes.wrapper}>
+        <Input
+          label="인증번호를 입력해주세요"
+          placeholder="6자리 인증번호"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          errMsg={errMsg}
+          successMsg={successMsg}
         />
+        <div className={stlyes.check}>
+          {!isTimeOver && <div className={stlyes.timer}>{formatTime()}</div>}
+          <ButtonOnClick
+            type="filled"
+            text="다시 받기"
+            color={isLoading || !isTimeOver ? 'disabled' : 'lightblue'} // 시간개념들어가야함
+            width="normal"
+            size="small"
+            disabled={isLoading || !isTimeOver} // 시간개념들어가야함
+            onClick={handleGetAuthCode}
+          />
+        </div>
       </div>
-
-      {/* 다음 버튼 */}
-      <div className={styles.submit}>
+      <div className={stlyes.submit}>
         <ButtonOnClick
           type="filled"
           text="다음"
-          color={isValid ? 'blue' : 'disabled'}
+          color={validate ? 'blue' : 'disabled'}
           width="full"
           size="large"
-          onClick={() => handleButtonClick()}
+          disabled={!validate || isPending}
+          onClick={handleButtonClick}
         />
       </div>
-    </div>
+    </>
   );
 }
---
+++ hook/useTimer.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect, useCallback } from 'react';
+
+// useTimer 커스텀 훅
+export function useTimer(initialTime = 5) {
+  const [timer, setTimer] = useState(initialTime);
+
+  useEffect(() => {
+    let intervalId: any;
+    if (timer > 0) {
+      intervalId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [timer]);
+
+  const resetTimer = useCallback(() => {
+    setTimer(initialTime);
+  }, [initialTime]);
+
+  // 시간을 mm:ss 포맷으로 변환하는 함수
+  const formatTime = useCallback(() => {
+    const minutes = Math.floor(timer / 60);
+    const seconds = timer % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  }, [timer]);
+
+  const isTimeOver = timer === 0;
+
+  return { timer, isTimeOver, resetTimer, formatTime };
+}
