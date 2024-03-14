0983b65cf47ffb242e10793adda89bcd76d16512
okhan1998 <whwlgns111@gmail.com>
2024-03-15 01:14:01 +0900

    feat:(오칸코드적용)  phoneNumberPage에 useInput 적용

--- components/register/registerFunnel/phoneNumberPage.tsx
+++ components/register/registerFunnel/phoneNumberPage.tsx
@@ -1,56 +1,59 @@
 'use client';
-import Input from '../../common/input';
-import styles from './index.module.scss';
-import ButtonOnClick from '../../common/button/buttonOnClick';
-import React from 'react';
-import usePhoneNumberValidation from '@/hook/usePhoneNumberValidation';
+import { useCallback } from 'react';
+import Input from '@/components/common/input';
+import stlyes from './index.module.scss';
+import ButtonOnClick from '@/components/common/button/buttonOnClick';
+import { useRecoilState } from 'recoil';
+import { phoneNumberState } from '@/recoil/atoms/registerState';
+import { useInput } from '@/hook/useInput';
 import { useSendAuthCode } from '@/hook/useRegister';
+import { validatePhonenumber } from '@/utils/validations/userValidations';
 
-interface PhoneNumberPageProps {
-  onNext: (data: string) => void;
-  data: string;
-}
-
-export default function phoneNumberPage({ onNext, data }: PhoneNumberPageProps) {
-  const { number, setNumber, isValid, errorMessage } = usePhoneNumberValidation(data);
+export default function Phonenumber({ onNext }: any) {
+  const [phoneNumber, setPhoneNumber] = useRecoilState(phoneNumberState);
+  const { value, setValue, errMsg, setErrMsg, validate } = useInput(
+    phoneNumber,
+    validatePhonenumber,
+  );
   const { mutate: sendAuthCode, isPending } = useSendAuthCode();
 
-  const handleButtonClick = () => {
-    sendAuthCode(number, {
-      onSuccess: (response) => {
-        console.log('인증코드 전송 성공: ', response.data.result);
-        onNext(number);
+  const handleButtonClick = useCallback(() => {
+    sendAuthCode(value, {
+      onSuccess: (res) => {
+        console.log(res);
+        setPhoneNumber(value);
+        onNext();
       },
-      onError: (error) => {
-        console.log('인증코드 전송 실패: ', error);
+      onError: (error: any) => {
+        console.log(error);
+        setErrMsg('전화번호 인증 중 오류가 발생했습니다.');
       },
     });
-  };
+  }, [value]);
 
   return (
-    <div className={styles.wrapper}>
-      {/* 휴대폰 번호 입력 */}
-      <Input
-        label="휴대폰 번호를 입력해주세요"
-        placeholder="010-1234-5678"
-        value={number}
-        onChange={(e) => setNumber(e.target.value)}
-        errMsg={errorMessage}
-        disabled={isPending}
-        autoFocus={true}
-      />
-
-      {/* 다음 버튼 */}
-      <div className={styles.submit}>
-        <ButtonOnClick
-          type="filled"
-          text="인증번호 받기"
-          color={isValid ? 'blue' : 'disabled'}
-          width="full"
-          size="large"
-          onClick={() => handleButtonClick()}
+    <>
+      <div className={stlyes.wrapper}>
+        <Input
+          label="휴대폰 번호를 입력해주세요"
+          placeholder="010-1234-1234"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          errMsg={errMsg}
+          autoFocus={true}
         />
       </div>
-    </div>
+      <div className={stlyes.submit}>
+        <ButtonOnClick
+          type="filled"
+          text="다음"
+          color={validate && !isPending ? 'blue' : 'disabled'}
+          width="full"
+          size="large"
+          disabled={!validate || isPending}
+          onClick={handleButtonClick}
+        />
+      </div>
+    </>
   );
 }
