217e8e5846945ae84cb50601492e861bcc18796a
okhan1998 <whwlgns111@gmail.com>
2024-03-15 00:10:31 +0900

    feat:(오칸코드적용)  useInput생성, validations utils추가, input컴포넌트 스타일 변경

---
+++ hook/useInput.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+export function useInput(initialValue: any, validateSync: any) {
+  const [value, setValue] = useState(initialValue);
+  const [errMsg, setErrMsg] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
+  const [validate, setValidate] = useState(false);
+
+  // 동기 유효성 검사 실행
+  useEffect(() => {
+    if (validateSync && value !== '') {
+      const result = validateSync(value, setErrMsg, setValue, setSuccessMsg);
+      setValidate(result);
+    }
+  }, [value, validateSync]);
+
+  return { value, setValue, errMsg, setErrMsg, successMsg, setSuccessMsg, validate };
+}
