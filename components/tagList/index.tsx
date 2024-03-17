898f14f6fc1b9dbe5de6526c0df75f20b3c8ee5d
yeonah98 <dbdusdkaa@naver.com>
2024-03-17 20:55:21 +0900

    feat: tag에 key값 추가

--- components/tagList/index.tsx
+++ components/tagList/index.tsx
@@ -18,7 +18,7 @@ export default function TagList({ info }: TagListProps) {
 
   // 간편결제 유무에 따른 태그 추가
   if (info.easyPayAvailable) {
-    tags.push(<Tag type="easyPay" content="간편결제" />);
+    tags.push(<Tag key="easyPay" type="easyPay" content="간편결제" />);
   }
 
   // D-Day 계산에 따른 태그 추가를 위한 함수
@@ -31,10 +31,10 @@ export default function TagList({ info }: TagListProps) {
     if (isDateToday(startDate) || isDatePast(startDate)) {
       // 시작 날짜가 오늘이거나 지났을 경우에만 D-Day 계산
       if (isDateToday(endDate)) {
-        tags.push(<Tag type={tagType} content={`${tagName} D-Day`} />);
+        tags.push(<Tag key={tagType} type={tagType} content={`${tagName} D-Day`} />);
       } else if (!isDatePast(endDate)) {
         const dayDiff = calculateDayDiff(endDate);
-        tags.push(<Tag type={tagType} content={`${tagName} D-${dayDiff}`} />);
+        tags.push(<Tag key={tagType} type={tagType} content={`${tagName} D-${dayDiff}`} />);
       }
     }
   };
@@ -52,7 +52,7 @@ export default function TagList({ info }: TagListProps) {
 
   // 신청마감 태그 추가 (대회 신청 및 단독출전 조정기간 모두 지났을 경우)
   if (areBothDatesPassed(info.registrationEndDate, info.soloRegistrationAdjustmentEndDate)) {
-    tags.push(<Tag type="deadline" content="신청마감" />);
+    tags.push(<Tag key="deadline" type="deadline" content="신청마감" />);
   }
 
   return <div className={styles.tagListWrapper}>{tags}</div>;
