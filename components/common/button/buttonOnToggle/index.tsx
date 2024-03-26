1e8c798e8b56047d545fe162c67c5d737e98cb2c
yeonah98 <dbdusdkaa@naver.com>
2024-03-26 15:54:30 +0900

    feat: buttonOnToggle 버튼 타입 직관적으로 수정

--- components/common/button/buttonOnToggle/index.tsx
+++ components/common/button/buttonOnToggle/index.tsx
@@ -2,33 +2,35 @@
 import styles from './index.module.scss';
 import clsx from 'clsx';
 
-// 버튼 타입
-type ButtonType = 'tap' | 'tag' | 'reaction';
-
-// 각 버튼 타입에 따른 색상 정의
-type TapTagColor = 'black';
-type ReactionColor = 'pink' | 'infoBlue';
-
-// 조건부 타입을 사용하여 색상을 버튼 타입에 따라 결정
-type ButtonColor<T> = T extends 'reaction' ? ReactionColor : TapTagColor;
-
-interface BaseButtonProps<T extends ButtonType> {
-  type: T;
-  color?: ButtonColor<T>; // 조건부 타입 사용
+// 기본 버튼 타입 정의
+interface BaseButtonProps {
   iconLeft?: React.ReactNode;
   text: string;
   isToggled?: boolean;
   onToggle: () => void;
 }
 
-export default function ButtonOnToggle<T extends ButtonType>({
-  type,
-  color = 'black' as ButtonColor<T>,
-  iconLeft,
-  text,
-  isToggled = false,
-  onToggle,
-}: BaseButtonProps<T>) {
+// 각 버튼 타입에 따른 타입 정의
+interface TagButtonProps extends BaseButtonProps {
+  type: 'tag';
+  color?: 'black';
+}
+
+interface TapButtonProps extends BaseButtonProps {
+  type: 'tap';
+  color?: 'black';
+}
+
+interface ReactionButtonProps extends BaseButtonProps {
+  type: 'reaction';
+  color: 'pink' | 'infoBlue';
+}
+
+// 버튼 타입 정의
+type ButtonProps = TagButtonProps | TapButtonProps | ReactionButtonProps;
+
+export default function ButtonOnToggle(props: ButtonProps) {
+  const { type, color = 'black', iconLeft, text, isToggled, onToggle } = props;
   return (
     <button
       className={clsx(styles.wrapper, styles[type], styles[color], {
