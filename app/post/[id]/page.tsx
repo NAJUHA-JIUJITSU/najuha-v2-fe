27b9819ea9ccfd5fdc6407f7a44be2927fe6367b
okhan1998 <whwlgns111@gmail.com>
2024-03-30 18:28:10 +0900

    feat:post 컴포넌트에서 패치하는게 아닌 props를 통해 postinfo 받아온거 렌더링하게끔 구조변경

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -4,11 +4,34 @@ import { ButtonIconNavigateBefore } from '@/components/common/icon/iconOnClick';
 import Post from '@/components/post';
 import stlyes from './index.module.scss';
 
+async function getPost(id: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        id,
+        nickname: '닉네임이다 이짜식아',
+        title:
+          '제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데 ', // 따로 글자제한이 필요해보임
+        type: 'free',
+        date: new Date(),
+        likeCnt: 1234,
+        viewCnt: 4558,
+        commentCnt: 27,
+        content:
+          '게시물 내용은 여기에 한 줄 미리보기로 표시할건데 여기선 전문을 다보여줍니다ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ게시물 내용은 여기에 한 줄 미리보기로 표시할건데 여기선 전문을 다보여줍니다ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ게시물 내용은 여기에 한 줄 미리보기로 표시할건데 여기선 전문을 다보여줍니다ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ',
+        hot: true,
+        profile: '/images/samplePoster1.png',
+      });
+    }, 1000);
+  });
+}
+
 export default async function post(props: any) {
+  const data = await getPost(props.params.id);
   return (
     <div className={stlyes.wrapper}>
       <Header leftIcon={<ButtonIconNavigateBefore />} title="글페이지" />
-      <Post id={props.params.id} />
+      <Post postInfo={data} />
     </div>
   );
 }
--- components/post/index.tsx
+++ components/post/index.tsx
@@ -1,49 +1,43 @@
 import styles from './index.module.scss';
 import Reaction from '@/components/reaction';
 
-async function getPost(id: number) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        id,
-        nickname: '닉네임이다 이짜식아',
-        title:
-          '제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데제목이다 이짜식아 제목이길어지면 어쩔껀데 ', // 따로 글자제한이 필요해보임
-        type: 'free',
-        date: new Date(),
-        likeCnt: 1234,
-        viewCnt: 4558,
-        commentCnt: 27,
-        content:
-          '게시물 내용은 여기에 한 줄 미리보기로 표시할건데 여기선 전문을 다보여줍니다ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ게시물 내용은 여기에 한 줄 미리보기로 표시할건데 여기선 전문을 다보여줍니다ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ게시물 내용은 여기에 한 줄 미리보기로 표시할건데 여기선 전문을 다보여줍니다ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ',
-        hot: true,
-        profile: '/images/samplePoster1.png',
-      });
-    }, 1000);
-  });
+// we get postinfo from parent component as props
+// we don't need to fetch postinfo from server
+// we can use postInfo directly
+// set postInfo type
+interface PostInfo {
+  id: number;
+  nickname: string;
+  title: string;
+  type: 'free' | 'seminar' | 'competition';
+  date: Date;
+  likeCnt: number;
+  viewCnt: number;
+  commentCnt: number;
+  content: string;
+  hot: boolean;
+  profile: string;
 }
-
-export default async function Post({ id }: { id: number }) {
-  const data = await getPost(id);
+export default async function Post({ postInfo }: { postInfo: PostInfo }) {
   return (
     <div className={styles.wrapper}>
       <div className={styles.top}>
         <div className={styles.left}>
-          <img className={styles.profile} src={data.profile} alt="profile" />
+          <img className={styles.profile} src={postInfo.profile} alt="profile" />
           <div className={styles.info}>
-            <div className={styles.name}>{data.nickname}</div>
+            <div className={styles.name}>{postInfo.nickname}</div>
             <div className={styles.date}>1년 전(수정됨)</div>
           </div>
         </div>
         <div className={styles.right}>
-          <div className={styles.caption}>조회 {data.viewCnt}</div>
+          <div className={styles.caption}>조회 {postInfo.viewCnt}</div>
         </div>
       </div>
       <div className={styles.middle}>
-        <div className={styles.title}>{data.title}</div>
-        <div className={styles.content}>{data.content}</div>
+        <div className={styles.title}>{postInfo.title}</div>
+        <div className={styles.content}>{postInfo.content}</div>
       </div>
-      <Reaction id={data.id} likeCnt={data.likeCnt} commentCnt={data.commentCnt} />
+      <Reaction id={postInfo.id} likeCnt={postInfo.likeCnt} commentCnt={postInfo.commentCnt} />
     </div>
   );
 }
