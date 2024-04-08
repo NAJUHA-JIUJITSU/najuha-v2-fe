f3fb8c3f41b9edebf04baf3aacf7ab8e5cbbed62
yeonah98 <dbdusdkaa@naver.com>
2024-04-08 15:26:58 +0900

    fix: 정렬 및 필터된 대회 가져올 수 있도록 쿼리 수정

--- components/competitionList/index.tsx
+++ components/competitionList/index.tsx
@@ -1,73 +1,7 @@
 'use client';
-
 import styles from './index.module.scss';
 import Card from '@/components/card';
-import { useGetCompetitions, useGetFilteredCompetitions } from '@/hooks/competition';
-
-const competitionList = [
-  {
-    earlybirdDiscountSnapshots: [
-      {
-        id: 1,
-        createdAt: '2024-03-01',
-        earlybirdStartDate: '2024-03-10',
-        earlybirdEndDate: '2024-03-20',
-        discountAmount: 0,
-        competitionId: 0,
-      },
-    ],
-    id: 1,
-    status: 'ACTIVE',
-    createdAt: '2024-03-01',
-    updatedAt: '2024-03-25',
-    title: '제 1회 대한민국 개발자 대회',
-    address: '서울특별시 강남구 테헤란로',
-    competitionDate: '2024-04-15',
-    registrationStartDate: '2024-03-10',
-    registrationEndDate: '2024-03-31',
-    refundDeadlineDate: '2024-04-01',
-    soloRegistrationAdjustmentStartDate: '2024-04-02',
-    soloRegistrationAdjustmentEndDate: '2024-04-05',
-    registrationListOpenDate: '2024-04-06',
-    bracketOpenDate: '2024-04-08',
-    description:
-      '대한민국 개발자들을 위한 대회로, 다양한 분야에서 실력을 겨루는 이벤트입니다. 참가자들은 최신 기술 트렌드와 혁신적인 아이디어를 공유할 수 있는 기회를 가집니다.',
-    isPartnership: true,
-    viewCount: 1234,
-    posterImgUrlKey: '/images/samplePoster1.png',
-  },
-  {
-    earlybirdDiscountSnapshots: [
-      {
-        id: 2,
-        createdAt: '2024-03-01',
-        earlybirdStartDate: '2024-03-10',
-        earlybirdEndDate: '2024-03-20',
-        discountAmount: 0,
-        competitionId: 0,
-      },
-    ],
-    id: 2,
-    status: 'ACTIVE',
-    createdAt: '2024-03-01',
-    updatedAt: '2024-03-25',
-    title: '제 1회 대한민국 개발자 대회',
-    address: '서울특별시 강남구 테헤란로',
-    competitionDate: '2024-04-15',
-    registrationStartDate: '2024-03-10',
-    registrationEndDate: '2024-03-31',
-    refundDeadlineDate: '2024-04-01',
-    soloRegistrationAdjustmentStartDate: '2024-04-02',
-    soloRegistrationAdjustmentEndDate: '2024-04-05',
-    registrationListOpenDate: '2024-04-06',
-    bracketOpenDate: '2024-04-08',
-    description:
-      '대한민국 개발자들을 위한 대회로, 다양한 분야에서 실력을 겨루는 이벤트입니다. 참가자들은 최신 기술 트렌드와 혁신적인 아이디어를 공유할 수 있는 기회를 가집니다.',
-    isPartnership: true,
-    viewCount: 1234,
-    posterImgUrlKey: '/images/samplePoster1.png',
-  },
-];
+import { useGetFilteredCompetitions } from '@/hooks/competition';
 
 interface CompetitionListProps {
   dateFilter: string;
@@ -82,14 +16,18 @@ export default function CompetitionList({
   selectOption,
   sortOption,
 }: CompetitionListProps) {
-  const { data: competitionListHook } = useGetFilteredCompetitions(
-    dateFilter,
-    locationFilter,
-    selectOption,
-    sortOption,
-  ); //todo: 초기값 설정 필요
+  const {
+    data: competitionList = [],
+    isLoading,
+    isError,
+  } = useGetFilteredCompetitions(dateFilter, locationFilter, selectOption, sortOption);
 
-  console.log('대회 리스트: ', competitionListHook);
+  console.log('대회 리스트: ', competitionList);
+  console.log('정렬: ', sortOption);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError) return <div>대회정보가 없습니다.</div>;
 
   return (
     <div className={styles.wrapper}>
