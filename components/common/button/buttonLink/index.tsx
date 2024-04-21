034a7eaf61cd9100e303795d97b9d3b0e8e89287
yeonah98 <dbdusdkaa@naver.com>
2024-04-21 14:39:34 +0900

    feat: buttonOnLink 컴포넌트에 하단바 고정 스타일 프롭스 추가

--- components/common/button/buttonLink/index.tsx
+++ components/common/button/buttonLink/index.tsx
@@ -1,14 +1,19 @@
 import styles from '../button.module.scss';
 import Link from 'next/link';
+import clsx from 'clsx';
 
 type ButtonType = 'filled' | 'outlined' | 'text' | 'underlined' | 'caption';
 type ButtonSize = 'small' | 'medium' | 'large' | 'xLarge';
 type ButtonColor = 'blue' | 'lightblue' | 'black' | 'gray' | 'disabled';
+type Width = 'full' | 'normal';
+type Position = 'fixed' | 'normal';
 //todo: disabled 처리하기
 
 interface BaseButtonProps {
   type: ButtonType;
   size: ButtonSize;
+  width?: Width;
+  position?: Position;
   color: ButtonColor;
   iconLeft?: React.ReactNode;
   text: string;
@@ -21,16 +26,27 @@ export default function ButtonLink({
   text,
   iconRight,
   size,
+  width = 'normal',
+  position = 'normal',
   type,
   color,
   href,
 }: BaseButtonProps) {
   const typeColor = `${type}-${color}`;
   return (
-    <Link className={`${styles.wrapper} ${styles[typeColor]} ${styles[size]}`} href={href}>
-      {iconLeft && <div className={styles.svg}>{iconLeft}</div>}
-      <div>{text}</div>
-      {iconRight && <div className={styles.svg}>{iconRight}</div>}
-    </Link>
+    <div
+      className={clsx({
+        [styles.fixed]: position === 'fixed',
+      })}
+    >
+      <Link
+        className={`${styles.wrapper} ${styles[typeColor]} ${styles[size]} ${styles[width]}`}
+        href={href}
+      >
+        {iconLeft && <div className={styles.svg}>{iconLeft}</div>}
+        <div>{text}</div>
+        {iconRight && <div className={styles.svg}>{iconRight}</div>}
+      </Link>
+    </div>
   );
 }
