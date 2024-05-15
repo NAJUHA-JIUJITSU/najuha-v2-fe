4b1b0527beee982b1810cb5579813a6500da683d
yeonah98 <dbdusdkaa@naver.com>
2024-05-15 18:42:39 +0900

    feat: 신청내역 페이지 틀 완성

---
+++ app/applicationList/page.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import styles from './index.module.scss';
+import { useState } from 'react';
+import Header from '@/components/common/header/Header';
+import { IconLinkSearch, IconLinkAlarm } from '@/components/common/icon/iconLink';
+import TabList from '@/components/tapList';
+import ButtonOnToggle from '@/components/common/button/buttonOnToggle';
+
+// TabKey 타입 정의
+type TabKey = 'competiton' | 'seminar' | 'openmat';
+
+// Tabs 배열 상수 정의
+const Tabs: TabKey[] = ['competiton', 'seminar', 'openmat'];
+
+// TabLabel 상수 정의
+const TabLabels: Record<TabKey, string> = {
+  competiton: '대회',
+  seminar: '세미나',
+  openmat: '오픈매트',
+};
+
+// tagBtnOptions 배열 상수 정의
+const tagBtnOptions = ['전체', '결제완료', '미결제', '환불'];
+
+export default function applicationList() {
+  const [selectedTab, setSelectedTab] = useState<TabKey>('competiton');
+  const [selectOptionsState, setSelectOptionsState] = useState<string>('전체');
+
+  return (
+    <div className={styles.wrapper}>
+      <Header title={'신청내역'} rightIcon1={<IconLinkAlarm />} rightIcon2={<IconLinkSearch />} />
+      <TabList<TabKey>
+        tabs={Tabs}
+        tabLabels={TabLabels}
+        selectedTab={selectedTab}
+        onSelectTab={setSelectedTab}
+      />
+      <div className={styles.tagBtnOptionsWrapper}>
+        {tagBtnOptions.map((option) => (
+          <ButtonOnToggle
+            key={option}
+            type="tag"
+            text={option}
+            isToggled={selectOptionsState === option}
+            onToggle={() => setSelectOptionsState(option)}
+          />
+        ))}
+      </div>
+      <div>
+        <div
+          style={{
+            lineHeight: 4,
+            fontSize: 18,
+          }}
+        >
+          {TabLabels[selectedTab]}의 {selectOptionsState} 상태의 리스트 불러오기
+        </div>
+        신청내역 카드들
+      </div>
+    </div>
+  );
+}
