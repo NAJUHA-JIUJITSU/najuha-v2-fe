ec019648f8e5144caf50ca4c684ce596603ee464
yeonah98 <dbdusdkaa@naver.com>
2024-05-20 15:43:27 +0900

    refactor: applicationList는 서버컴포넌트로 유지하고, ApplicationListContent만 클라이언트 컴포넌트로 분리

---
+++ components/applicationList/applicationListContent/index.tsx
@@ -0,0 +1,56 @@
+'use client';
+import styles from './index.module.scss';
+import { useState } from 'react';
+import TabList from '@/components/tapList';
+import ButtonOnToggle from '@/components/common/button/buttonOnToggle';
+import ApplicantProgramCard from '@/components/programCard/applicantProgramCard';
+
+// TabKey 타입 정의
+type TabKey = 'competiton' | 'seminar' | 'openmat';
+
+// Tabs 배열 상수 정의
+const Tabs: TabKey[] = ['competiton', 'seminar', 'openmat'];
+
+// TabLabel 상수 정의
+const TabLabels: Record<TabKey, string> = {
+  competiton: '대회',
+  seminar: '세미나',
+  openmat: '오픈매트',
+};
+
+// tagBtnOptions 배열 상수 정의
+const tagBtnOptions = ['전체', '결제완료', '미결제', '환불'];
+
+export default function applicationListPage() {
+  const [selectedTab, setSelectedTab] = useState<TabKey>('competiton');
+  const [selectOptionsState, setSelectOptionsState] = useState<string>('전체');
+
+  return (
+    <div className={styles.wrapper}>
+      <TabList<TabKey>
+        tabs={Tabs}
+        tabLabels={TabLabels}
+        selectedTab={selectedTab}
+        onSelectTab={setSelectedTab}
+      />
+      <div className={styles.tagBtnOptionsWrapper}>
+        {tagBtnOptions.map((option) => (
+          <ButtonOnToggle
+            key={option}
+            type="tag"
+            text={option}
+            isToggled={selectOptionsState === option}
+            onToggle={() => setSelectOptionsState(option)}
+          />
+        ))}
+      </div>
+      <div className={styles.applicationCardList}>
+        <p>
+          {TabLabels[selectedTab]}의 {selectOptionsState} 상태의 리스트 불러오기
+        </p>
+        <ApplicantProgramCard />
+        <ApplicantProgramCard />
+      </div>
+    </div>
+  );
+}
