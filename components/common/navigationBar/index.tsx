a03685b73cbcf19cfea395c9391e0bfc16073084
yeonah98 <dbdusdkaa@naver.com>
2024-05-23 18:15:52 +0900

    feat: navigationBar 로그인/로그아웃 기능 추가

--- components/common/navigationBar/index.tsx
+++ components/common/navigationBar/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 import styles from './index.module.scss';
+import { useState, useEffect } from 'react';
 import IconLogo from '@/public/svgs/logo.svg';
 import IconHome from '@/public/svgs/home.svg';
 import IconBook from '@/public/svgs/book.svg';
@@ -8,11 +9,33 @@ import IconCard from '@/public/svgs/card.svg';
 import IconProfile from '@/public/svgs/profile.svg';
 import { usePathname } from 'next/navigation';
 import NavigationMenu from './navigationMenu';
+import Cookies from 'js-cookie';
 
 export default function navigationBar() {
+  // 로그인 여부 관리
+  const [isLogin, setIsLogin] = useState(false);
+
+  // 쿠키에서 토큰 가져오기
+  const token = Cookies.get('accessToken');
+
+  // 로그아웃 버튼 클릭 시 쿠키 삭제
+  const logout = () => {
+    // 확인 대화상자 표시
+    const confirmLogout = confirm(
+      '작성 중인 정보가 있다면 모두 사라지며 홈으로 돌아가게 됩니다. 정말로 로그아웃 하시겠습니까?',
+    );
+    if (confirmLogout) {
+      Cookies.remove('accessToken');
+      Cookies.remove('refreshToken');
+      setIsLogin(false);
+      // 홈으로 이동
+      window.location.href = '/'; //todo: 최적화 필요
+    }
+  };
+
   //현재 url 확인 후 해당 메뉴 스타일 변경
   const currentPath = usePathname();
-  const logoItem = { href: '/', icon: <IconLogo />, label: '홈' };
+  const logoItem = { href: '/', icon: <IconLogo />, label: '로고' };
   const menuItems = [
     { href: '/', icon: <IconHome />, label: '홈' },
     { href: '/community', icon: <IconBook />, label: '게시판' },
@@ -27,11 +50,15 @@ export default function navigationBar() {
   ];
   const loginItems = [
     { href: '/login', label: '로그인' },
-    { href: '/logout', label: '로그아웃' },
+    { label: '로그아웃', isButton: true, onClick: logout },
   ];
 
-  //로그인 여부 확인
-  const isLogin = false;
+  // userInfoSelector에서 가져온 값으로 로그인 여부 확인
+  useEffect(() => {
+    if (token) {
+      setIsLogin(true);
+    }
+  }, []);
 
   return (
     <div className={styles.wrapper}>
@@ -60,7 +87,8 @@ export default function navigationBar() {
             key={loginItems[1].label}
             href={loginItems[1].href}
             label={loginItems[1].label}
-            isButton={true}
+            isButton={loginItems[1].isButton}
+            onClick={loginItems[1].onClick}
             isActive={false}
           />
         ) : (
@@ -68,7 +96,6 @@ export default function navigationBar() {
             key={loginItems[0].label}
             href={loginItems[0].href}
             label={loginItems[0].label}
-            isButton={true}
             isActive={false}
           />
         )}
