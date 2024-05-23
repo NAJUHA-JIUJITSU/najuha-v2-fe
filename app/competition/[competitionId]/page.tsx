847fe8a75117be4d28385a4323521c859084c786
yeonah98 <dbdusdkaa@naver.com>
2024-05-23 18:27:10 +0900

    design: competitionId 페이지 wrapper 수정

--- app/competition/[competitionId]/page.tsx
+++ app/competition/[competitionId]/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import styles from './index.module.scss';
 import Header from '@/components/common/header/Header';
 import { ButtonIconNavigateBefore } from '@/components/common/icon/iconOnClick';
