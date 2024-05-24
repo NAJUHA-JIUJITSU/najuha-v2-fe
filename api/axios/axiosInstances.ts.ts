bd31104313fea82dd19f01b65b28873c3bbecb85
hermin9804 <hermin9804@gmail.com>
2024-05-24 15:03:20 +0900

    NJH-376 feat: axios array query param format &T=a&T=b 형식으로 변경

--- api/axios/axiosInstances.ts.ts
+++ api/axios/axiosInstances.ts.ts
@@ -1,12 +1,21 @@
 import axios, { AxiosInstance } from 'axios';
+import qs from 'qs';
 
+// axiosPublic 인스턴스 생성
 export const axiosPublic: AxiosInstance = axios.create({
   baseURL: 'http://localhost:3001/',
+  paramsSerializer: (params) => {
+    return qs.stringify(params, { arrayFormat: 'repeat' });
+  },
 });
 
+// axiosPrivate 인스턴스 생성
 export const axiosPrivate: AxiosInstance = axios.create({
   baseURL: 'http://localhost:3001/',
   headers: {
     'Content-Type': 'application/json',
+  },
+  paramsSerializer: (params) => {
+    return qs.stringify(params, { arrayFormat: 'repeat' });
   },
 });
