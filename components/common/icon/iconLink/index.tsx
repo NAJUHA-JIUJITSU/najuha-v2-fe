b81d8c9bc28d5f349566f26e474b1f076a2e6097
yeonah98 <dbdusdkaa@naver.com>
2024-05-28 15:37:22 +0900

    feat: iconLink에 홈페이지에서 사용할 바로가기 아이콘 추가 및 스타일 설정

--- components/common/icon/iconLink/index.tsx
+++ components/common/icon/iconLink/index.tsx
@@ -3,6 +3,10 @@ import Link from 'next/link';
 import IconAlarm from '@/public/svgs/alarm.svg';
 import IconLogo from '@/public/svgs/logo.svg';
 import IconSearch from '@/public/svgs/search.svg';
+import IconThropy from '@/public/svgs/trophy.svg';
+import IconSeminar from '@/public/svgs/seminar.svg';
+import IconOpenmat from '@/public/svgs/openmat.svg';
+import IconEvent from '@/public/svgs/event.svg';
 
 interface Props {
   icon: React.ReactNode;
@@ -25,4 +29,49 @@ function IconLinkSearch() {
   return <IconLink icon={<IconSearch />} redirectUrl={'/search'} />;
 }
 
-export { IconLink, IconLinkAlarm, IconLinkLogo, IconLinkSearch };
+function IconLinkThropy() {
+  return (
+    <div className={styles.programBtn}>
+      <IconLink icon={<IconThropy />} redirectUrl={'/competition'} />
+      <p className={styles.programBtnText}>대회</p>
+    </div>
+  );
+}
+
+function IconLinkSeminar() {
+  return (
+    <div className={styles.programBtn}>
+      <IconLink icon={<IconSeminar />} redirectUrl={'/seminar'} />
+      <p className={styles.programBtnText}>세미나</p>
+    </div>
+  );
+}
+
+function IconLinkOpenmat() {
+  return (
+    <div className={styles.programBtn}>
+      <IconLink icon={<IconOpenmat />} redirectUrl={'/openmat'} />
+      <p className={styles.programBtnText}>오픈매트</p>
+    </div>
+  );
+}
+
+function IconLinkEvent() {
+  return (
+    <div className={styles.programBtn}>
+      <IconLink icon={<IconEvent />} redirectUrl={'/event'} />
+      <p className={styles.programBtnText}>이벤트</p>
+    </div>
+  );
+}
+
+export {
+  IconLink,
+  IconLinkAlarm,
+  IconLinkLogo,
+  IconLinkSearch,
+  IconLinkThropy,
+  IconLinkSeminar,
+  IconLinkOpenmat,
+  IconLinkEvent,
+};
