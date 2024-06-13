a3cc2c7dacec6b27931708286b455d05c8cf1381
okhan1998 <whwlgns111@gmail.com>
2024-06-13 16:05:25 +0900

    feat: nestia sdk refresh로직 구현

--- api/nestia/common.ts
+++ api/nestia/common.ts
@@ -1,6 +1,7 @@
 import Cookies from 'js-cookie';
 import { IConnection } from '@nestia/fetcher';
-import { postRefreshToken } from '@/api/authApi';
+import { postRefreshToken } from '@/api/nestia/authApi';
+import { AllErrorTypes } from 'najuha-v2-api/lib/common/response/errorResponse';
 
 export const createConnection = (): IConnection => {
   const token = Cookies.get('accessToken');
@@ -25,21 +26,45 @@ export const handleRefreshToken = async () => {
 
 export const withAuth = async <T>(apiCall: (connection: IConnection) => Promise<T>): Promise<T> => {
   const connection: IConnection = createConnection();
+
   try {
     return await apiCall(connection);
   } catch (error) {
-    // eslint 경고를 제거할지 안할지 모르겠음.. 해당 타입에러가 이해가 안간다.
-    // error분기 의도된에러 의도되지않은에러로 나누는게 좋을듯
-    // 그거를 어떻게 나눌지
-    // 만약 의도된 에러면 에러핸들함수에 보내주면되고, 보내주고 받을때 너가 만들어준 타입을 받아서 처리하면 될듯.
-    if (error.response?.data?.code === 1000 || error.response?.data?.code === 1001) {
-      const newAccessToken = await handleRefreshToken();
-      if (newAccessToken) {
-        connection.headers = connection.headers || {};
-        connection.headers['Authorization'] = `Bearer ${newAccessToken}`;
-        return await apiCall(connection);
+    let parsedError: any;
+
+    if (error instanceof Error && error.message) {
+      try {
+        parsedError = JSON.parse(error.message); // 에러 메시지를 파싱하여 객체로 변환
+      } catch (e) {
+        throw error; // JSON 파싱에 실패하면 원래 에러를 그대로 던짐
+      }
+    } else {
+      throw error; // 에러 객체가 Error 인스턴스가 아니거나 메시지가 없으면 원래 에러를 그대로 던짐
+    }
+
+    console.log('parsedError: ', parsedError);
+    if (isAllErrorTypes(parsedError)) {
+      if (parsedError.code === 1000 || parsedError.code === 1001) {
+        const newAccessToken = await handleRefreshToken();
+        if (newAccessToken) {
+          connection.headers = connection.headers || {};
+          connection.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return await apiCall(connection);
+        }
       }
     }
-    throw error; // (1000, 1001코드를 제외한 모든 에러를 관리)
+    throw parsedError; // (1000, 1001 코드를 제외한 모든 에러를 관리)
   }
 };
+
+function isAllErrorTypes(error: any): error is AllErrorTypes {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    typeof error.isSuccess === 'boolean' &&
+    typeof error.status === 'number' &&
+    typeof error.code === 'number' &&
+    typeof error.type === 'string' &&
+    typeof error.result === 'string'
+  );
+}
