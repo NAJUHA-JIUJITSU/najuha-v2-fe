5bbeb7645a56bd8f671b0cd14cb73ff57bcf23ed
yeonah98 <dbdusdkaa@naver.com>
2024-06-21 20:01:35 +0900

    refactor: layout 컴포넌트 스타일 파일 하나로 통일

--- components/layout/baseLayout/index.tsx
+++ components/layout/baseLayout/index.tsx
@@ -1,17 +1,18 @@
 import { ReactNode } from 'react';
-import styles from './index.module.scss';
+import styles from '../index.module.scss';
 import Footer from '@/components/common/footer';
 
 interface baseLayoutProps {
   children: ReactNode;
+  isFooter?: boolean;
 }
 
-export default function baseLayout({ children }: baseLayoutProps) {
+export default function baseLayout({ children, isFooter = true }: baseLayoutProps) {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.children}>{children}</div>
-        <Footer />
+        {isFooter && <Footer />}
       </div>
     </div>
   );
--- components/layout/navigationLayout/index.tsx
+++ components/layout/navigationLayout/index.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import NavigationBar from '@/components/common/navigationBar';
-import styles from './index.module.scss';
+import styles from '../index.module.scss';
 import Footer from '@/components/common/footer';
 
 interface navigationLayoutProps {
@@ -11,7 +11,7 @@ export default function navigationLayout({ children }: navigationLayoutProps) {
   return (
     <div className={styles.container}>
       <NavigationBar />
-      <div className={styles.content}>
+      <div className={`${styles.content} ${styles.navigationContent}`}>
         <div className={styles.children}>{children}</div>
         <Footer />
       </div>
