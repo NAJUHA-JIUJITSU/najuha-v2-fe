6e82ef9c60ca981bbf7c300357ea0a417c91c18c
yeonah98 <dbdusdkaa@naver.com>
2024-06-23 20:15:21 +0900

    fix: PostEditor 외부클릭 범위 변경

--- components/createPost/postEditor/index.tsx
+++ components/createPost/postEditor/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import useOutsideClick from '@/hooks/useOutsideClick';
 import Input from '@/components/common/input';
 import styles from './index.module.scss';
@@ -11,43 +11,55 @@ interface PostEditorProps {
 function PostEditor({ onChange, formData }: PostEditorProps) {
   const [title, setTitle] = useState(formData.title);
   const [content, setContent] = useState(formData.content);
-  const wrapperRef = useRef<HTMLFormElement | null>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
 
-  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const adjustHeight = useCallback(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, []);
+
+  const handleTitleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
-  };
+  }, []);
 
-  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleContentChange = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
-  };
+  }, []);
 
-  const handleFormChange = () => {
+  const handleFormChange = useCallback(() => {
     onChange({ title, content });
-  };
+  }, [title, content, onChange]);
 
-  useOutsideClick(wrapperRef, handleFormChange, 'mousedown');
+  useEffect(() => {
+    adjustHeight(); // 컴포넌트가 마운트될 때 높이를 조절합니다.
+  }, [title, content, adjustHeight]); // title, content가 변경될 때마다 높이를 조절합니다.
+
+  useOutsideClick(divRef, handleFormChange, 'mousedown');
 
   return (
-    <form ref={wrapperRef} className={styles.wrapper}>
+    <div className={styles.wrapper} ref={divRef}>
       <Input
         placeholder="제목을 입력하세요"
         type="text"
         isUnderline={true}
-        autoFocus={true}
         width="full"
         isMsg={false}
         value={title}
         onChange={handleTitleChange}
       />
       <textarea
+        ref={textareaRef}
         className={styles.textareaWrapper}
         placeholder="자유롭게 의견을 공유해보세요!"
         name="content"
         value={content}
         onChange={handleContentChange}
       ></textarea>
-    </form>
+    </div>
   );
 }
 
-export default PostEditor;
+export default React.memo(PostEditor);
