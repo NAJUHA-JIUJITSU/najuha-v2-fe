c8c52213f952b18c10166670b9201e341cda207d
yeonah98 <dbdusdkaa@naver.com>
2024-06-23 20:55:39 +0900

    feat: ButtonIconNavigateClear 추가

--- components/common/icon/iconOnClick/index.tsx
+++ components/common/icon/iconOnClick/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react';
 import IconNavigateBefore from '@/public/svgs/navigateBefore.svg';
+import IconClear from '@/public/svgs/clear.svg';
 import useGoBack from '@/hooks/useGoBack';
 
 interface Props {
@@ -18,4 +19,18 @@ function ButtonIconNavigateBefore() {
   return <ButtonIcon icon={<IconNavigateBefore />} onClick={goBack}></ButtonIcon>;
 }
 
-export { ButtonIcon, ButtonIconNavigateBefore };
+function ButtonIconNavigateClear() {
+  const goBack = useGoBack();
+  const handleButtonClick = () => {
+    const userConfirmed = window.confirm(
+      '페이지를 벗어나면 작성 중인 내용이 모두 사라져요. 그래도 이동하시겠어요?',
+    );
+    if (userConfirmed) {
+      goBack();
+    }
+  };
+
+  return <ButtonIcon icon={<IconClear />} onClick={handleButtonClick}></ButtonIcon>;
+}
+
+export { ButtonIcon, ButtonIconNavigateBefore, ButtonIconNavigateClear };
