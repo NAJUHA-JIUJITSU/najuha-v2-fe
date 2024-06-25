640a2d2e392d8874be6a2175cc4a177090a689d9
yeonah98 <dbdusdkaa@naver.com>
2024-06-25 20:15:52 +0900

    feat: 게시글 조회 무한스크롤 훅 생성

--- api/nestia/postApi.ts
+++ api/nestia/postApi.ts
@@ -1,6 +1,9 @@
 import { withAuth } from '@/api/nestia/common';
 import api from 'najuha-v2-api/lib/api';
-import { CreatePostReqBody } from 'najuha-v2-api/lib/modules/posts/presentation/posts.controller.dto';
+import {
+  CreatePostReqBody,
+  FindPostsReqQuery,
+} from 'najuha-v2-api/lib/modules/posts/presentation/posts.controller.dto';
 
 // u-7-1 createPost.
 // 새로운 게시글을 작성합니다.
@@ -11,6 +14,17 @@ const postCreatePost = async (data: CreatePostReqBody) => {
   return response.result;
 };
 
+// u-7-2 findPosts.
+// 게시글 목록을 조회합니다.
+const postFindPosts = async (data: FindPostsReqQuery) => {
+  const response = await withAuth((connection) =>
+    api.functional.user.posts.findPosts(connection, data),
+  );
+
+  return response.result;
+};
+
 export const postApi = {
   postCreatePost,
+  postFindPosts,
 };
--- hooks/post.ts
+++ hooks/post.ts
@@ -1,10 +1,13 @@
 import { IImageCreateDto } from 'najuha-v2-api/lib/modules/images/domain/interface/image.interface';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useInfiniteQuery } from '@tanstack/react-query';
 import { postApi } from '@/api/nestia/postApi';
 import { imageApi } from '@/api/nestia/imageApi';
-import { CreatePostReqBody } from 'najuha-v2-api/lib/modules/posts/presentation/posts.controller.dto';
+import {
+  CreatePostReqBody,
+  FindPostsReqQuery,
+} from 'najuha-v2-api/lib/modules/posts/presentation/posts.controller.dto';
 
-//이미지와 함께 게시글을 작성하는 훅
+// 이미지와 함께 게시글을 작성하는 훅
 const useCreatePostWithImages = (path: IImageCreateDto['path']) => {
   return useMutation({
     mutationFn: async ({ data, files }: { data: CreatePostReqBody; files: File[] }) => {
@@ -31,4 +34,17 @@ const useCreatePostWithImages = (path: IImageCreateDto['path']) => {
   });
 };
 
-export { useCreatePostWithImages };
+// 필터 및 정렬된 게시글 목록을 조회하는 훅
+const useFindPosts = (data: FindPostsReqQuery) => {
+  return useInfiniteQuery({
+    queryKey: ['posts', data],
+    queryFn: async ({ pageParam }: { pageParam: number }) => {
+      const response = await postApi.postFindPosts({ ...data, page: pageParam });
+      return response;
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  });
+};
+
+export { useCreatePostWithImages, useFindPosts };
